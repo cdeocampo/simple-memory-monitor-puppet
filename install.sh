4187782a084a9abd47b686991d0ae837cf1ae0c1
Christian Deocampo <christian.deocampo@gmail.com>
2018-03-22 23:03:55 +0800

    Adds additional commands to clone git repository

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+DIR_NAME='simple-memory-monitor-puppet'
+
 rpm -ivh http://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
 yes | yum -y install puppet
 hostname bpx.server.local
@@ -6,3 +8,12 @@ echo "bpx.server.local" >> /etc/resolv.conf
 echo "127.0.0.1     bpx.server.local" >> /etc/hosts
 sed -i -e 's/localhost.localdomain/bpx.server.local/' /etc/sysconfig/network
 service network restart
+
+# clone repo
+yum -y install git
+git clone https://github.com/cdeocampo/simple-memory-monitor-puppet.git ~/$DIR_NAME
+cp -R ~/$DIR_NAME/manifests /etc/puppet/
+cp -R ~/$DIR_NAME/modules /etc/puppet/
+cp ~/$DIR_NAME/hiera.yaml /etc/puppet/
+mkdir /etc/puppet/hieradata
+touch /etc/puppet/hieradata/common.yaml
